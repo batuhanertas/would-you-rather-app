04c73c83fe4672426a3c964bce59e82f6050ba42
Batuhan Ertas <batuhanertas@gmail.com>
2018-09-22 20:07:08 +0200

    feat: Show percentages of options

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
+function calculateVote (question) {
+    let firstOption = question.optionOne.votes.length
+    let secondOption = question.optionTwo.votes.length
+
+    return {
+        firstOptionPercentage : ((firstOption / (firstOption + secondOption)) * 100).toFixed(1),
+        secondOptionPercentage : ((secondOption/ (firstOption + secondOption)) * 100).toFixed(1),
+        firstOptionTotal: firstOption,
+        secondOptionTotal: secondOption,
+        allTotal: firstOption + secondOption
+    }
+}
+
 
 class QuestionPage extends Component {
     render() {
         const { question } = this.props
+
+        let { firstOptionPercentage, secondOptionPercentage, firstOptionTotal, secondOptionTotal, allTotal } 
+                = calculateVote(question)
+
         return (
             <div>
                 <h4> Results </h4>
                 <div>
                     <p>Would you rather { question.optionOne.text }?</p>
+                    <p>{firstOptionPercentage}%</p>
+                    <p>{firstOptionTotal} out of {allTotal}</p>
                 </div>
                 <div>
                     <p>Would you rather { question.optionTwo.text }?</p>
+                    <p>{secondOptionPercentage}%</p>
+                    <p>{secondOptionTotal} out of {allTotal}</p>
                 </div>
             </div>
         )
