a4585182c79f86bfa478387ff2229ea3235afb6a
Batuhan Ertas <batuhanertas@gmail.com>
2018-09-23 12:12:50 +0200

    feat: Add radio buttons for question submit

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,18 @@
+import { _saveQuestionAnswer } from "../utils/_DATA";
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const SUBMIT_QUESTION = 'SUBMIT_QUESTION'
 
 export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
+    }
+}
+
+function submitQuestion(question) {
+    return {
+        type: SUBMIT_QUESTION,
+        question
     }
 }
--- src/components/QuestionSubmit.js
+++ src/components/QuestionSubmit.js
@@ -1,12 +1,72 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom'
+import { handleSubmitQuestion } from '../actions/questions'
 
 class QuestionSubmit extends Component {
+    state = {
+        option: '',
+        toHome: false,
+    }
+
+    handleChange(option) {
+        this.setState(() => ({
+          option: option
+        }))
+    }
+
+    handleSubmit(authedUser, questionId) {
+        const { dispatch } = this.props
+
+        // TODO: handle submit
+        // dispatch(handleSubmitQuestion(authedUser, questionId, this.state.option))
+
+        this.setState(() => ({
+            toHome: true
+        }))
+    }
+ 
+
     render() {
+        const { question, authedUser } = this.props
+
+        const { toHome } = this.state
+
+        if (toHome === true) {
+          return <Redirect to='/home' />
+        }
+
         return (
-            <h4>Question Submit</h4>
+            <div>
+                <h3>{question.author} asks:</h3>
+                <h4>Would you rather...</h4>
+                <form>
+                    <label>
+                        <input type="radio" name="option" value="optionOne" 
+                        onChange={(e) => this.handleChange(e.target.value)} />
+                        {question.optionOne.text}
+                    </label>
+                    <br />
+                    <label>
+                        <input type="radio" name="option" value="optionTwo" 
+                        onChange={(e) => this.handleChange(e.target.value)}
+                        />
+                        {question.optionTwo.text}
+                    </label>
+                    <button onClick={() => this.handleSubmit(authedUser, question.id)}>Submit</button>
+                </form>
+            </div>
         )
     }
 }
 
-export default connect()(QuestionSubmit)
+function mapStateToProps ({ questions, authedUser }, id) {
+    const question = questions[id.id]
+    
+    return {
+        question: question,
+        authedUser: authedUser
+    }
+}
+
+export default connect(mapStateToProps)(QuestionSubmit)
