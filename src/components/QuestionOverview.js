9c66067c1e61c4c303abfd30ba5c56d07539084b
Batuhan Ertas <batuhanertas@gmail.com>
2018-10-04 21:27:26 +0200

    refactor: Refactor code according to review

--- src/components/QuestionOverview.js
+++ src/components/QuestionOverview.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 function calculateVote (question, user) {
@@ -17,41 +17,38 @@ function calculateVote (question, user) {
 }
 
 
-class QuestionPage extends Component {
-    render() {
-        const { question, authedUser, user } = this.props
+const QuestionOverview = ({question, authedUser, user}) => {
+    let { firstOptionPercentage, secondOptionPercentage, firstOptionTotal, secondOptionTotal, allTotal, firstOptionNotSelected } 
+            = calculateVote(question, authedUser)
 
-        let { firstOptionPercentage, secondOptionPercentage, firstOptionTotal, secondOptionTotal, allTotal, firstOptionNotSelected } 
-                = calculateVote(question, authedUser)
-
-        return (
-            <div className="card text-center question-container">
-                <div className="card-header">Asked by {user.name}</div>
-                <img className="card-img-top" src={user.avatarURL} alt={user.id} />
-                <h4> Results </h4>
-                <div className="card text-center">
-                    <div className="card-body">
-                        <p className="card-text">Would you rather { question.optionOne.text }?</p>
-                        <h5 className="card-title">{firstOptionTotal} out of {allTotal}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{firstOptionPercentage}%</h6>
-                        <h3 hidden={!firstOptionNotSelected}>
-                            <span className="badge badge-danger" >Your Vote!</span>
-                        </h3>
-                    </div>
-                </div>
-                <div className="card text-center">
-                    <div className="card-body">
-                        <p className="card-text">Would you rather { question.optionOne.text }?</p>
-                        <h5 className="card-title">{secondOptionTotal} out of {allTotal}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{secondOptionPercentage}%</h6>
-                        <h3 hidden={firstOptionNotSelected}>
-                            <span className="badge badge-danger" >Your Vote!</span>
-                        </h3>
-                    </div>
+    return (
+        <div className="card text-center question-container">
+            <div className="card-header">Asked by {user.name}</div>
+            <img className="card-img-top" src={user.avatarURL} alt={user.id} />
+            <h4> Results </h4>
+            <div className="card text-center">
+                <div className="card-body">
+                    <p className="card-text">Would you rather { question.optionOne.text }?</p>
+                    <h5 className="card-title">{firstOptionTotal} out of {allTotal}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{firstOptionPercentage}%</h6>
+                    <h3 hidden={!firstOptionNotSelected}>
+                        <span className="badge badge-danger" >Your Vote!</span>
+                    </h3>
                 </div>
             </div>
-        )
-    }
+            <div className="card text-center">
+                <div className="card-body">
+                    <p className="card-text">Would you rather { question.optionOne.text }?</p>
+                    <h5 className="card-title">{secondOptionTotal} out of {allTotal}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{secondOptionPercentage}%</h6>
+                    <h3 hidden={firstOptionNotSelected}>
+                        <span className="badge badge-danger" >Your Vote!</span>
+                    </h3>
+                </div>
+            </div>
+        </div>
+    )
+    
 }
 
 function mapStateToProps ({ questions, authedUser, users }, id) {
@@ -65,4 +62,4 @@ function mapStateToProps ({ questions, authedUser, users }, id) {
     }
 }
 
-export default connect(mapStateToProps)(QuestionPage)
+export default connect(mapStateToProps)(QuestionOverview)
--- src/components/User.js
+++ src/components/User.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
-class User extends Component {
-    render () {
-        const { user } = this.props
-        const answeredQuestions = Object.keys(user.answers).length
-        const createdQuestions = user.questions.length
+const User = ({user}) => {
+    const answeredQuestions = Object.keys(user.answers).length
+    const createdQuestions = user.questions.length
 
-        return (
-            <div className="card text-center question-container">
-                <div className="card-header">{user.name}</div>
-                <img src={user.avatarURL} className="card-img-top" alt={user.id} />
-                <h5 className="card-title">Score {answeredQuestions + createdQuestions}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Answered Question: {answeredQuestions}</h6>
-                <h6 className="card-subtitle mb-2 text-muted">Created Questions: {createdQuestions}</h6>
-            </div>
-        )
-    }
+    return (
+        <div className="card text-center question-container">
+            <div className="card-header">{user.name}</div>
+            <img src={user.avatarURL} className="card-img-top" alt={user.id} />
+            <h5 className="card-title">Score {answeredQuestions + createdQuestions}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Answered Question: {answeredQuestions}</h6>
+            <h6 className="card-subtitle mb-2 text-muted">Created Questions: {createdQuestions}</h6>
+        </div>
+    )
 }
 
 function mapStateToProps({ users }, {id}) {
